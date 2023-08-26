e330df499e479dff127530276725f66940fe0a36
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-08-25 21:53:08 -0300

    styles refactor

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	width: 100vw;
+	background: ${({ theme }) => theme.backgroundColor};
+	color: ${({ theme }) => theme.textColor};
 	display: flex;
 	justify-content: center;
 	align-items: center;
