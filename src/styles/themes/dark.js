88d374e36908fb988217071d594fd0cdf2164a3a
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-09-23 11:09:44 -0300

    blur propertys & arrow to toggle timer added, imports refactored

--- src/styles/themes/dark.js
+++ src/styles/themes/dark.js
@@ -1,8 +1,15 @@
-import arrow from '../../assets/images/icons/darkArrow.svg'
+import backArrow from '../../assets/images/icons/backDarkArrow.svg'
+import timerArrow from '../../assets/images/icons/darkTimerArrow.svg';
 
 export default {
 	backgroundColor: 'radial-gradient(circle at -8.9% 51.2%, rgb(255, 124, 0) 0%, rgb(255, 124, 0) 15.9%, rgb(255, 163, 77) 15.9%, rgb(255, 163, 77) 24.4%, rgb(19, 30, 37) 24.5%, rgb(19, 30, 37) 66%)',
 	textColor: '#ffffffde',
-	arrow,
-	error: '#F15152'
+	backArrow,
+	timerArrow,
+	error: '#F15152',
+	blur: `
+		background: #ffffff10;
+		backdrop-filter: blur(12px);
+		-webkit-backdrop-filter: blur(12px);
+	`
 }
--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -1,8 +1,15 @@
-import arrow from '../../assets/images/icons/lightArrow.svg';
+import backArrow from '../../assets/images/icons/backLightArrow.svg';
+import timerArrow from '../../assets/images/icons/lightTimerArrow.svg';
 
 export default {
 	backgroundColor: '#EBEBD3',
 	textColor: '#222',
-	arrow,
-	error: '#F15152'
+	backArrow,
+	timerArrow,
+	error: '#F15152',
+	blur: `
+		background: #00000010;
+		backdrop-filter: blur(12px);
+		-webkit-backdrop-filter: blur(12px);
+	`
 }
