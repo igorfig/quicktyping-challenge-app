769e8c8dfa04173a2bd310f48f4cd9d684021354
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-10-13 23:55:19 -0300

    phrases added

---
+++ src/data/phrases.js
@@ -0,0 +1,17 @@
+export default {
+	hard: [
+		"A vida é uma jornada repleta de desafios e surpresas. A cada passo, aprendemos lições valiosas que moldam nosso caminho. Devemos abraçar o presente, honrar o passado e enfrentar o futuro com coragem. A cada amanhecer, somos agraciados com uma nova oportunidade de crescimento e realização. Não importa quais obstáculos surjam, a determinação e a esperança nos impulsionarão adiante. O segredo da felicidade está em encontrar a beleza nas coisas simples, amar profundamente e compartilhar generosamente. Viver é uma dádiva preciosa, e cada dia é uma página em branco que podemos preencher com momentos significativos. Que nossa jornada seja repleta de amor, amizade e aventura, e que possamos trilhá-la com gratidão e alegria.",
+		"A natureza revela sua beleza de muitas formas, desde as cores vibrantes das flores até o som suave do vento nas árvores. Cada estação traz sua própria magia, do calor do verão ao aconchego do inverno. A terra é repleta de maravilhas naturais que merecem nossa admiração e proteção. À medida que exploramos o mundo ao nosso redor, descobrimos a complexidade e a harmonia da natureza. Devemos cuidar do meio ambiente, preservar sua diversidade e garantir um futuro sustentável para as gerações vindouras.",
+		"A tecnologia tem transformado nosso modo de vida, conectando pessoas de diferentes partes do mundo em um piscar de olhos. Hoje, temos acesso a informações e recursos incríveis na ponta dos dedos. A internet é uma janela para o conhecimento e a inovação, mas também traz desafios em termos de privacidade e segurança. À medida que avançamos na era digital, é importante equilibrar os benefícios com a responsabilidade. Devemos usar a tecnologia de forma consciente, proteger nossos dados e promover um ambiente online saudável."
+	],
+
+	normal: [
+		"A poesia é o espelho da alma humana. Ela reflete as nossas alegrias e tristezas, os nossos sonhos e desilusões, os nossos anseios mais profundos. Através da poesia, podemos dar voz ao indizível, expressar o inexpressável e compreender o incompreensível. Ela nos leva a uma jornada interior, explorando as paisagens da nossa mente e do nosso coração. A poesia é a ponte que nos conecta com a humanidade, uma linguagem universal que transcende barreiras e nos lembra do que significa ser humano.",
+		"O amor é uma decisão, é um ato de vontade, é um ato de compromisso, é uma escolha que fazemos em um momento e que continuamos fazendo, dia após dia, ano após ano. O amor não é algo que simplesmente acontece conosco, é algo que construímos e cultivamos, como um jardim que requer cuidado e atenção constantes. E, no final, é o amor que damos que mais importa, porque é o amor que recebemos em troca."
+	],
+
+	easy: [
+		"A vida, o sofrimento, a morte são janelas que se abrem, por onde o ser humano vê e sente a realidade das coisas. A poesia é a arte que nos faz ver, sentir e pensar através dessas janelas.",
+		"Não tente compreender as coisas. Apenas esteja com elas. Não tente controlá-las, porque se você tentar controlá-las, elas começarão a te controlar. E se você tentar compreendê-las, você se perderá."
+	]
+}
